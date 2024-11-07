cee1620d25e1b42c9ee67393d670a33ca2e446a5
Redhun <redhuns123@gmail.com>
2024-11-07 22:41:18 +0530

    JWT Filter added

--- src/main/java/com/redhun/spring_security_demo/config/SecurityConfig.java
+++ src/main/java/com/redhun/spring_security_demo/config/SecurityConfig.java
@@ -1,4 +1,5 @@
 package com.redhun.spring_security_demo.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,32 +25,36 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Autowired
-  private   UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private  JwtFilter jwtFilter;
 
     @Bean
-    public   AuthenticationProvider authProvider() {
+    public AuthenticationProvider authProvider() {
 
-     DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
-     provider.setUserDetailsService(userDetailsService);
-     provider.setPasswordEncoder(new BCryptPasswordEncoder());
-     return provider;
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable).
-    authorizeHttpRequests(request -> request.requestMatchers("register","login").permitAll()
-                .anyRequest()
-                .authenticated()).
+                authorizeHttpRequests(request -> request.requestMatchers("register", "login").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 
-   httpBasic(Customizer.withDefaults()).
-     sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        ;
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager (AuthenticationConfiguration config) throws Exception {
-        return  config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 //    @Bean
--- src/main/java/com/redhun/spring_security_demo/service/JwtService.java
+++ src/main/java/com/redhun/spring_security_demo/service/JwtService.java
@@ -1,21 +1,22 @@
 package com.redhun.spring_security_demo.service;
 
 
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -53,8 +54,41 @@ public class JwtService {
     }
 
 
-    private Key getKey() {
+    private SecretKey getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
+    public String extractUserName(String token) {
+        // extract the username from jwt token
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()                 // Create JwtParserBuilder
+                .verifyWith(getKey())            // Set the signing key
+                .build()                            // Build the parser
+                .parseSignedClaims(token)              // Parse the token
+                .getPayload();                         // Extract claims
+    }
+
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 }
